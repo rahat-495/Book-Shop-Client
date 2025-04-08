3b42d25a823afb475a77f806f40de341f0a30a92
rahat-495 <md8702687@gmail.com>
2025-04-08 21:43:35 +0600

    Main : Full redux are setup are done !

---
+++ src/redux/features/auth/authApi.ts
@@ -0,0 +1,16 @@
+
+import { baseApi } from "../../api/baseApi";
+
+const authApi = baseApi.injectEndpoints({
+    endpoints : (builder) => ({
+        login : builder.mutation({
+            query : (payload) => ({
+                url : "/auth/login",
+                method : "POST",
+                body : payload,
+            })
+        })
+    })
+})
+
+export const { useLoginMutation } = authApi ;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,10 @@
+
+const routes = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default routes;
