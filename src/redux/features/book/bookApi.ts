5102686f3713cd9ee9d363a9711ff4b63c575e02
rahat-495 <md8702687@gmail.com>
2025-04-10 20:18:09 +0600

    Rahat : creating book are done !

--- src/redux/features/book/bookApi.ts
+++ src/redux/features/book/bookApi.ts
@@ -3,6 +3,13 @@ import { baseApi } from "@/redux/api/baseApi";
 
 const bookApi = baseApi.injectEndpoints({
     endpoints : (builder) => ({
+        createBook : builder.mutation({
+            query : (payload) => ({
+                url : "/books/create-book",
+                method : "POST",
+                body : payload,
+            })
+        }),
         getAllBooks : builder.query({
             query : (params) => ({
                 url : "/books",
@@ -13,4 +20,4 @@ const bookApi = baseApi.injectEndpoints({
     })
 })
 
-export const { useGetAllBooksQuery } = bookApi ;
+export const { useGetAllBooksQuery , useCreateBookMutation } = bookApi ;
